4d0fb66384031de3e45177d19558a03d4b8578ad
Eric Wuerschmidt <ericwuerschmidt@gmail.com>
2019-08-01 23:05:55 -0400

    Added Spotify methods to retrieve tracks from playlist. Added method docs for clients.

--- Slack/SlackClient/SlackClient.py
+++ Slack/SlackClient/SlackClient.py
@@ -4,17 +4,21 @@ import slack
 class SlackClient():
 
 	def __init__(self):
+		'''Initializes an object with all necessary items to create a Slack Client'''
 		self.channel = '#sr-test'
 		self.client = None
 		self.client_token = os.environ['SLACK_OAUTH_TOKEN']
 
 	def connect(self):
+		'''Authentication for Slack Client'''
 		self.client = slack.WebClient(token=self.client_token)
 
 	def post_message(self, message, channel=None):
+		'''Writes specified message to either default or specified channel'''
 		response = self.client.chat_postMessage(channel=self.channel if not channel else channel, text=message)
 
 	def set_channel(self, channel):
+		'''Sets the channel for messages to be posted to'''
 		self.channel = channel
 
 if __name__ == "__main__":
--- Spotify/SpotifyClient/SpotifyClient.py
+++ Spotify/SpotifyClient/SpotifyClient.py
@@ -1,3 +1,5 @@
+import datetime
+from datetime import date
 import json
 import os
 import spotipy
@@ -6,53 +8,80 @@ import spotipy.util as util
 class SpotifyClient():
 
 	def __init__(self):
+		'''Initializes an object with all necessary items to create a Spotify Client'''
 		self.client = None
 		self.client_id = os.environ['SPOTIPY_CLIENT_ID']
 		self.client_secret = os.environ['SPOTIPY_CLIENT_SECRET']
+		self.fields_filter = None
 		self.redirect_uri = os.environ['SPOTIPY_REDIRECT_URI']
 		self.scope = 'playlist-read-collaborative playlist-read-private playlist-modify-private playlist-modify-public'
 		self.user_id = None
 		self.username = None
-
-	# Just storing this here from old driver
-	# def analysis(spotify_client):
-	#     playlists = spotify_client.current_user_playlists()
-	#     retrieved_id = 0
-	#     song_counter = {}
-	#     for playlist in playlists['items']:
-	#         if playlist['name'] == 'Song Roulette: July':
-	#             retrieved_id = playlist['id']
-	#     print(retrieved_id)
-	#     tracks = spotify_client.user_playlist_tracks(user_mapping['me']['id'], playlist_id=retrieved_id)
-	#     for track in tracks['items']:
-	#         if track['added_by']['id'] in song_counter:
-	#             song_counter[track['added_by']['id']]+=1
-	#         else:
-	#             song_counter[track['added_by']['id']]=1
-	#     print(song_counter)	
+		if self.user_id == None or self.username == None:
+			print("Check user ID and username in SpotifyClient!")
+			exit()
 
 	def connect(self):
+		'''Authentication for Spotify Client'''
 		token = util.prompt_for_user_token(self.username, 
 			self.scope, client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri)
 		self.client = spotipy.Spotify(auth=token) if token else None
 
 	def get_playlist_id(self, playlist_name):
-		playlist_id = -1
+		'''Returns a playlist ID for a given playlist name'''
+		pid = -1
 		all_playlists = self.client.current_user_playlists()
 		for playlist in all_playlists['items']:
 			if playlist['name'] == playlist_name:
-				playlist_id = playlist['id']
-		if playlist_id == -1:
+				pid = playlist['id']
+		if pid == -1:
 			print("Playlist with name '%s' not found!" % playlist_name)
 			#self.client.user_create_playlist(self.user_id, playlist_name)
 			exit()
-		return playlist_id
+		return pid
 
 	def get_playlist_url(self, playlist_name):
+		'''Returns a playlist URL for a given playlist name'''
 		playlist = self.client.user_playlist(self.user_id, self.get_playlist_id(playlist_name))
 		return playlist['external_urls']['spotify']
+
+	def get_current_tracks(self, playlist_name):
+		'''Returns the filtered track information of a given playlist name'''
+		pid = self.get_playlist_id(playlist_name)
+		counter = 0
+		track_count = 100
+		self.set_fields('items(added_at,added_by.id,track(name,popularity,uri))')
+		all_tracks = []
+		while track_count == 100:
+			tracks = self.client.user_playlist_tracks(self.user_id, pid, fields=self.fields_filter, offset=100*counter)
+			all_tracks += tracks['items']
+			track_count = len(tracks['items'])
+			counter+=1
+		return all_tracks
+
+	#TODO: Add year confirmation to this method to avoid multiple months being returned
+	def get_month_tracks(self, playlist_name, month):
+		'''Returns the filtered track information of a given playlist name for tracks added in a specific month'''
+		if type(month) is str:
+			print("Please provide month as an integer")
+			exit()
+		pid = self.get_playlist_id(playlist_name)
+		counter = 0
+		track_count = 100
+		self.set_fields('items(added_at,track(name,popularity,uri))')
+		tracks = self.client.user_playlist_tracks(self.user_id, self.get_playlist_id(playlist_name), fields=self.fields_filter)
+		filtered_tracks = []
+		while track_count == 100:
+			tracks = self.client.user_playlist_tracks(self.user_id, pid, fields=self.fields_filter, offset=100*counter)
+			for track in tracks['items']:
+				if datetime.datetime.strptime(track['added_at'], '%Y-%m-%dT%H:%M:%SZ').month == month:
+					filtered_tracks.append(track)
+			track_count = len(tracks['items'])
+			counter+=1
+		return filtered_tracks
 		
 	def move_tracks(self, from_playlist, to_playlist):
+		'''Move all tracks from one playlist to another'''
 		from_id = self.get_playlist_id(from_playlist)
 		to_id = self.get_playlist_id(to_playlist)
 		tracks_to_move = []
@@ -63,10 +92,28 @@ class SpotifyClient():
 		self.client.user_playlist_remove_all_occurrences_of_tracks(self.user_id, from_id, tracks_to_move)
 
 	def rename_playlist(self, old_playlist_name, new_playlist_name):
+		'''Renames a playlist given old and new playlists'''
 		self.client.user_playlist_change_details(self.user_id, self.get_playlist_id(old_playlist_name), new_playlist_name)
+
+	def set_fields(self, fields):
+		'''Set the fields parameter used when making REST requests'''
+		self.fields_filter = fields
+
+	def track_count_per_user(self, playlist_name):
+		'''Returns the amount of songs per user that were added to a playlist'''
+	    pid = self.client.get_playlist_id(playlist_name)
+	    song_counter = {}
+	    tracks = spotify_client.user_playlist_tracks(self.user_id, playlist_id=retrieved_id)
+	    for track in tracks['items']:
+	        if track['added_by']['id'] in song_counter:
+	            song_counter[track['added_by']['id']]+=1
+	        else:
+	            song_counter[track['added_by']['id']]=1
+	    return song_counter
 
 if __name__ == "__main__":
 	my_client = SpotifyClient()
 	my_client.connect()
-	my_client.get_playlist_url("Song Roulette: July")
+	tracks = my_client.get_month_tracks("Song Roulette: All Songs", 1)
+	print(tracks)
 	exit()

018a50739952f10f4a79e83077941c2804d16fcf
Eric Wuerschmidt <ericwuerschmidt@gmail.com>
2019-09-17 22:41:32 -0400

    Added db scripts for working with tables. Method for adding db partitions

--- Database/DatabaseClient/DatabaseClient.py
+++ Database/DatabaseClient/DatabaseClient.py
@@ -1,8 +1,10 @@
 import os
 import psycopg2
+import re
 
 class DatabaseClient:
     """Class to integrate with Heroku Postgres DB"""
+    partition_map = {'playlist_track_count': r'playlist_track_count_y\d{4}m\d{1,2}'}
 
     def __init__(self):
         """Initializes an object with all necessary items to create a Database Client"""
@@ -20,14 +22,28 @@ class DatabaseClient:
         self.connection = psycopg2.connect(self.db_url, sslmode='require')
         self.cursor = self.connection.cursor()
 
+    def create_partition(self, partition_name, table, start_val, end_val):
+        """Create a new partition for given table """
+        if re.match(self.partition_map[table], partition_name):
+            query = f"CREATE TABLE {partition_name} PARTITION OF {table} FOR VALUES FROM ({start_val}) TO ({end_val}); COMMIT;"
+            self.execute(query)
+        else:
+            print(f"Partition name {partition_name} does not match pattern {self.partition_map[table]}")
+            exit()
+
     def execute(self, query):
         """Executes and returns results from provided query"""
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
+    def update_schema(self, schema):
+        """Set database schema"""
+        self.execute(f"set search_path to {schema}")
+
 if __name__ == "__main__":
     myDb = DatabaseClient()
     myDb.connect()
     print(myDb.execute('select version();'))
+    myDb.create_partition('playlist_track_count_1', 'playlist_track_count', '2019-10-01', '2019-11-01')
     exit()
     

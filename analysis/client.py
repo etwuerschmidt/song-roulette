15025a9ecba141ea65d5db622d0829e68c6ee583
Eric Wuerschmidt <ericwuerschmidt@gmail.com>
2020-03-28 15:02:32 -0700

    Save plots in Chart Studio. Analysis now responds with figures instead of raw data

--- analysis/client.py
+++ analysis/client.py
@@ -1,4 +1,6 @@
 from calendar import monthrange
+import chart_studio
+import chart_studio.plotly as plot
 import collections
 import datetime
 from datetime import date
@@ -6,28 +8,31 @@ import logging
 import os
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go 
+import plotly.graph_objects as go
 import subprocess
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
+
 class Plotter():
     """Class to plot a variety of graphs"""
-    
-    def __init__(self, display=False, save=False):
+
+    def __init__(self, display=False, host_online=True):
         self.display = display
-        self.save = save
-        if self.save:
+        self.host_online = host_online
+        if not self.host_online:
             try:
-                subprocess.run(["orca", "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+                subprocess.run(["orca", "--version"], stdout=subprocess.DEVNULL,
+                               stderr=subprocess.DEVNULL, check=True)
             except:
-                print("Orca is not installed or is not in PATH! Saving images is not supported. Setting Plotter to display.")
-                self.save = False
-                self.display = True
+                print("Orca is not installed or is not in PATH! Saving images locally is not supported. Setting Plotter to host images online.")
+                self.host_online = True
+        chart_studio.tools.set_credentials_file(
+            username='etwuerschmidt', api_key=os.environ['CHART_STUDIO_KEY'])
 
     def bar_graph(self, x, y, xaxis=None, yaxis=None, title='bar_graph'):
         """Create a bar graph with provided information"""
-        fig = go.Figure([go.Bar(x=list(x), 
+        fig = go.Figure([go.Bar(x=list(x),
                                 y=list(y),
                                 text=list(y),
                                 textposition='auto')])
@@ -35,25 +40,32 @@ class Plotter():
                           xaxis_title=xaxis,
                           yaxis_title=yaxis,
                           yaxis={'tickformat': ',d'})
-        self.graph_view_save(fig)
+        return self.graph_view_save(fig)
 
     def graph_view_save(self, fig):
+        image_name = fig.layout['title']['text'].replace(
+            " ", "_").replace(":", "_").replace("/", "_")
+        url = None
         if self.display:
             fig.show()
-        if self.save:
+        if self.host_online:
+            url = plot.plot(fig, filename=image_name, auto_open=False)
+            logging.info(f"Image successfully created and hosted at {url}")
+        else:
             if not os.path.exists("images"):
                 os.mkdir("images")
-            image_name = fig.layout['title']['text'].replace(" ", "_").replace(":", "_")
             fig.write_image(f"images/{image_name}.png")
+        return url
 
     def line_graph(self, x, y, xaxis=None, yaxis=None, title='line_graph'):
         """Create a line graph with provided information"""
+        logging.info(f"Creating line graph with title {title}")
         fig = go.Figure(data=go.Scatter(x=list(x), y=list(y)))
         fig.update_layout(title=title,
                           xaxis_title=xaxis,
                           yaxis_title=yaxis,
                           yaxis={'tickformat': ',d'})
-        self.graph_view_save(fig)
+        return self.graph_view_save(fig)
 
     def radar_graph(self, data, title='radar_graph'):
         """Create a radar graph with provided information"""
@@ -61,7 +73,7 @@ class Plotter():
         fig = px.line_polar(df, r='r', theta='theta', line_close=True)
         fig.update_traces(fill='toself')
         fig.update_layout(title=title)
-        self.graph_view_save(fig)
+        return self.graph_view_save(fig)
 
 
 def avg_audio_features(song_features):
@@ -82,22 +94,26 @@ def avg_audio_features(song_features):
         avg_features[key] = value / len(song_features)
     return avg_features
 
+
 def track_count_per_day(playlist_items, pad_to_today=False, pad_to_month_end=False):
     """Returns the amount of songs added to a playlist for each day of a month"""
     """One month of a playlist is passed in"""
     logging.info("Calculating track count per day")
-    prev_track_date = datetime.datetime.strptime(playlist_items[0]['added_at'], '%Y-%m-%dT%H:%M:%SZ')
+    prev_track_date = datetime.datetime.strptime(
+        playlist_items[0]['added_at'], '%Y-%m-%dT%H:%M:%SZ')
     day_song_counter = {}
-    days_in_curr_month = list(range(1, monthrange(prev_track_date.year, prev_track_date.month)[1] + 1))
+    days_in_curr_month = list(
+        range(1, monthrange(prev_track_date.year, prev_track_date.month)[1] + 1))
     for track in playlist_items:
-        track_date = datetime.datetime.strptime(track['added_at'], '%Y-%m-%dT%H:%M:%SZ')
+        track_date = datetime.datetime.strptime(
+            track['added_at'], '%Y-%m-%dT%H:%M:%SZ')
         while len(days_in_curr_month) > 0 and track_date.day > days_in_curr_month[0]:
             day_song_counter[f"{track_date.month}/{days_in_curr_month[0]}"] = 0
             days_in_curr_month.pop(0)
         if f"{track_date.month}/{track_date.day}" in day_song_counter:
             day_song_counter[f"{track_date.month}/{track_date.day}"] += 1
-        else: 
-            day_song_counter [f"{track_date.month}/{track_date.day}"] = 1
+        else:
+            day_song_counter[f"{track_date.month}/{track_date.day}"] = 1
             days_in_curr_month.remove(track_date.day)
         prev_track_date = track_date
     if pad_to_today or pad_to_month_end:
@@ -105,21 +121,25 @@ def track_count_per_day(playlist_items, pad_to_today=False, pad_to_month_end=Fal
         if pad_to_today:
             day_range = list(range(prev_track_date.day+1, date.today().day+1))
         elif pad_to_month_end:
-            day_range = list(range(prev_track_date.day+1, monthrange(prev_track_date.year, prev_track_date.month)[1] + 1))
+            day_range = list(range(
+                prev_track_date.day+1, monthrange(prev_track_date.year, prev_track_date.month)[1] + 1))
         while day_range:
             day_song_counter[f"{prev_track_date.month}/{day_range[0]}"] = 0
             day_range.pop(0)
     return day_song_counter
 
+
 def track_count_per_month(playlist_items, all_songs_sr_analysis=False, pad_to_today=False):
     """Returns the amount of songs added to a playlist for each month"""
     """Full playlist is passed in"""
     logging.info("Calculating track count per month")
-    prev_track_date = datetime.datetime.strptime(playlist_items[0]['added_at'], '%Y-%m-%dT%H:%M:%SZ')
+    prev_track_date = datetime.datetime.strptime(
+        playlist_items[0]['added_at'], '%Y-%m-%dT%H:%M:%SZ')
     month_song_counter = {}
     month_range = list(range(prev_track_date.month, 13))
     for track in playlist_items:
-        track_date = datetime.datetime.strptime(track['added_at'], '%Y-%m-%dT%H:%M:%SZ')
+        track_date = datetime.datetime.strptime(
+            track['added_at'], '%Y-%m-%dT%H:%M:%SZ')
         prev_track_year = prev_track_date.year
         while track_date.year != prev_track_year:
             while month_range:
@@ -146,11 +166,13 @@ def track_count_per_month(playlist_items, all_songs_sr_analysis=False, pad_to_to
                 month_range.pop(0)
             prev_track_year += 1
             month_range = list(range(1, 13))
-        month_range = list(range(prev_track_date.month+1, date.today().month+1)) if not year_pad else list(range(1, date.today().month+1))
+        month_range = list(range(prev_track_date.month+1, date.today().month+1)
+                           ) if not year_pad else list(range(1, date.today().month+1))
         while month_range:
             month_song_counter[f"{month_range[0]}/{date.today().year}"] = 0
             month_range.pop(0)
     return month_song_counter
+
 
 def track_count_per_user(playlist_items):
     """Returns the amount of songs per user that were added to a playlist"""
@@ -161,10 +183,12 @@ def track_count_per_user(playlist_items):
             user_song_counter[track['added_by']['id']] += 1
         else:
             user_song_counter[track['added_by']['id']] = 1
-    user_song_counter = collections.OrderedDict(sorted(user_song_counter.items(), key=lambda item: item[1], reverse=True))
+    user_song_counter = collections.OrderedDict(
+        sorted(user_song_counter.items(), key=lambda item: item[1], reverse=True))
     return user_song_counter
 
+
 if __name__ == "__main__":
-    myPlot = Plotter(save=True)
-    myPlot.line_graph([1,2,3], [1,2,3], title='Test Plot')
+    myPlot = Plotter()
+    url = myPlot.line_graph([1, 2, 3], [1, 2, 3], title='Test Plot')
     exit()
--- backend.py
+++ backend.py
@@ -13,27 +13,32 @@ from spotify.client import SpotifyClient
 from threading import Thread
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+date_format = "%m/%d/%Y %I:%M:%S %p %Z"
+
 
 def connect_clients(*args):
     print("Connecting clients...")
     for client in args:
         client.connect()
 
-sp_client = SpotifyClient(user_id=1269825738, username='Eric Wuerschmidt', SPOTIPY_CLIENT_ID=os.environ['SPOTIPY_CLIENT_ID'], 
-                            SPOTIPY_CLIENT_SECRET=os.environ['SPOTIPY_CLIENT_SECRET'])
+
+sp_client = SpotifyClient(user_id=1269825738, username='Eric Wuerschmidt', SPOTIPY_CLIENT_ID=os.environ['SPOTIPY_CLIENT_ID'],
+                          SPOTIPY_CLIENT_SECRET=os.environ['SPOTIPY_CLIENT_SECRET'])
 connect_clients(sp_client)
-graph_draw = AnalysisClient.Plotter(save=True)
+graph_draw = AnalysisClient.Plotter()
 app = Flask(__name__)
+
 
 def all_analysis(response_url, playlist_name):
     user_analysis(response_url, playlist_name)
     date_analysis(response_url, playlist_name)
     properties_analysis(response_url, playlist_name)
 
-def user_analysis(response_url, playlist_name):    
+
+def user_analysis(response_url, playlist_name):
     month_tracks = sp_client.get_playlist_tracks(playlist_name)
     with open('users.json', 'r') as json_file:
-        user_info = json.load(json_file)    
+        user_info = json.load(json_file)
     user_count = AnalysisClient.track_count_per_user(month_tracks)
     user_count_by_name = {}
     slack_usernames = ""
@@ -44,29 +49,77 @@ def user_analysis(response_url, playlist_name):
         else:
             user_count_by_name[user_info[user]['name']] = count
             slack_usernames += f"<{user_info[user]['slack_id']}> "
-    user_graph_title = f"{playlist_name} Tracks Added per User"
-    graph_draw.bar_graph(user_count_by_name.keys(), user_count_by_name.values(), title=user_graph_title, xaxis='User', yaxis='Songs Added')
+    current_time = datetime.datetime.now().strftime(date_format).strip()
+    user_graph_title = f"{playlist_name} Tracks Added per User - {current_time}"
+    image_url = graph_draw.bar_graph(user_count_by_name.keys(), user_count_by_name.values(
+    ), title=user_graph_title, xaxis='User', yaxis='Songs Added')
 
-    data = {
+    logging.info("Responding to user analysis request")
+
+    text_response_data = json.dumps({
         "response_type": "in_channel",
-        "text": f"Here are all our contributors for {playlist_name}: {slack_usernames}"
-    }
-    requests.post(response_url, json=data)
+        "text": f"Thanks to all the contributors for this month! {slack_usernames}"
+    })
+    requests.post(response_url, data=text_response_data)
 
-def date_analysis(response_url, playlist_name):
+    image_data = json.dumps({
+        "response_type": "in_channel",
+        "text": f"Whoops! Looks like something went wrong with displaying the user data",
+        "blocks": [
+            {
+                "type": "image",
+                "title": {
+                    "type": "plain_text",
+                    "text": "Date frequency plot"
+                },
+                "image_url": image_url[:-1] + '.png',
+                "alt_text": "This month's user data"
+            }
+        ]
+    })
+    requests.post(response_url, data=image_data)
+    logging.info("Request completed")
+
+
+def date_analysis(response_url, playlist_name, today_pad=True):
     month_tracks = sp_client.get_playlist_tracks(playlist_name)
-    day_count = AnalysisClient.track_count_per_day(month_tracks, pad_to_month_end=True)
+    day_count = AnalysisClient.track_count_per_day(
+        month_tracks, pad_to_today=today_pad)
     day_freq_message = ""
     for day, count in day_count.items():
         day_freq_message += f"{day}: {count} songs \n"
-    day_graph_title = f"{playlist_name} Tracks Added by Day"
-    graph_draw.line_graph(day_count.keys(), day_count.values(), title=day_graph_title, xaxis='Day of month', yaxis='Songs Added')
+    current_time = datetime.datetime.now().strftime(date_format).strip()
+    day_graph_title = f"{playlist_name} Tracks Added by Day - {current_time}"
+    image_url = graph_draw.line_graph(day_count.keys(), day_count.values(
+    ), title=day_graph_title, xaxis='Day of month', yaxis='Songs Added')
 
-    data = {
+    logging.info("Responding to data analysis request")
+
+    text_response_data = json.dumps({
         "response_type": "in_channel",
-        "text": f"Here are the songs that have been added per day for {playlist_name}: \n {day_freq_message}"
-    }
-    requests.post(response_url, json=data)
+        "text": f"Here are the songs that have been added per day for `{playlist_name}`"
+    })
+    requests.post(response_url, data=text_response_data)
+
+    image_data = json.dumps({
+        "response_type": "in_channel",
+        "text": f"Whoops! Looks like something went wrong with displaying the date data",
+        "blocks": [
+            {
+                "type": "image",
+                "title": {
+                    "type": "plain_text",
+                    "text": "Date frequency plot"
+                },
+                "image_url": image_url[:-1] + '.png',
+                "alt_text": "This month's date data"
+            }
+        ]
+    })
+
+    response = requests.post(response_url, data=image_data)
+    logging.info("Request completed")
+
 
 def properties_analysis(response_url, playlist_name):
     month_tracks = sp_client.get_playlist_tracks(playlist_name)
@@ -76,17 +129,41 @@ def properties_analysis(response_url, playlist_name):
     features_message = ""
     for feature, quan in avg_audio_features.items():
         features_message += f"{feature}: {quan} \n"
-    features_graph_title = f"{playlist_name} Audio Features"
-    graph_draw.radar_graph(avg_audio_features, title=features_graph_title)
-    
-    data = {
+    current_time = datetime.datetime.now().strftime(date_format).strip()
+    features_graph_title = f"{playlist_name} Audio Features - {current_time}"
+    image_url = graph_draw.radar_graph(
+        avg_audio_features, title=features_graph_title)
+
+    logging.info("Responding to properties analysis request")
+
+    text_response_data = json.dumps({
         "response_type": "in_channel",
-        "text": f"Here are the audio characteristics of {playlist_name}: \n {features_message}"
-    }
-    requests.post(response_url, json=data)
+        "text": f"Here are the audio characteristics of `{playlist_name}`"
+    })
+    requests.post(response_url, data=text_response_data)
+
+    image_data = json.dumps({
+        "response_type": "in_channel",
+        "text": f"Whoops! Looks like something went wrong with displaying the properties data",
+        "blocks": [
+            {
+                "type": "image",
+                "title": {
+                    "type": "plain_text",
+                    "text": "Audio analysis plot"
+                },
+                "image_url": image_url[:-1] + '.png',
+                "alt_text": "This month's properties data"
+            }
+        ]
+    })
+    requests.post(response_url, data=image_data)
+    logging.info("Request completed")
+
 
 def run_server():
     app.run()
+
 
 def set_playlist_names(request):
     test = False
@@ -109,11 +186,13 @@ def set_playlist_names(request):
 
     return (request_info, old_playlist_name, all_playlist_name, new_playlist_name)
 
+
 def valid_request(request):
     valid_token = request.form['token'] == os.environ['SLACK_REQUEST_TOKEN']
     team_id = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
     if not (valid_token and team_id):
         abort(400)
+
 
 def valid_user(request):
     if not request.form['user_id'] == os.environ['SLACK_BOT_ADMIN']:
@@ -121,35 +200,46 @@ def valid_user(request):
             text=f"Sorry, only <@{os.environ['SLACK_BOT_ADMIN']}> has access to this command right now."
         )
 
+
 @app.route('/refresh', methods=['POST'])
 def refresh():
+    logging.info(f"Received a refresh request from {request.form['user_id']}")
     valid_request(request)
     valid_user(request)
-    request_type, old_playlist_name, all_playlist_name, new_playlist_name = set_playlist_names(request)
+    request_type, old_playlist_name, all_playlist_name, new_playlist_name = set_playlist_names(
+        request)
     sp_client.refresh_access()
     return jsonify(
         response_type="in_channel",
         text="You made a refresh request!"
     )
 
+
 @app.route('/analysis', methods=['POST'])
 def analysis():
+    logging.info(
+        f"Received an analysis request from {request.form['user_id']}")
     valid_request(request)
     valid_user(request)
-    analysis_type, old_playlist_name, all_playlist_name, new_playlist_name = set_playlist_names(request)
+    analysis_type, old_playlist_name, all_playlist_name, new_playlist_name = set_playlist_names(
+        request)
     sp_client.refresh_access()
 
     if analysis_type == "users":
-        worker_thread = Thread(target=user_analysis, args=(request.form['response_url'], new_playlist_name, ))
+        worker_thread = Thread(target=user_analysis, args=(
+            request.form['response_url'], new_playlist_name, ))
         worker_thread.start()
     elif analysis_type == "dates":
-        worker_thread = Thread(target=date_analysis, args=(request.form['response_url'], new_playlist_name, ))
+        worker_thread = Thread(target=date_analysis, args=(
+            request.form['response_url'], new_playlist_name, ))
         worker_thread.start()
     elif analysis_type == "properties":
-        worker_thread = Thread(target=properties_analysis, args=(request.form['response_url'], new_playlist_name, ))
+        worker_thread = Thread(target=properties_analysis, args=(
+            request.form['response_url'], new_playlist_name, ))
         worker_thread.start()
     elif analysis_type == "all":
-        worker_thread = Thread(target=all_analysis, args=(request.form['response_url'], new_playlist_name, ))
+        worker_thread = Thread(target=all_analysis, args=(
+            request.form['response_url'], new_playlist_name, ))
         worker_thread.start()
     else:
         return jsonify(
@@ -161,6 +251,7 @@ def analysis():
         response_type="in_channel",
         text=f"Processing your analysis request for `{analysis_type}` now - this may take a little bit"
     )
+
 
 if __name__ == "__main__":
     # db_client = DatabaseClient(DATABASE_URL=secret_info['DATABASE_URL'])

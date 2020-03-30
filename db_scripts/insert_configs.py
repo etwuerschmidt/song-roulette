f86d5c686329dc7b9c63ca78e8b7b3560cf12778
Eric Wuerschmidt <ericwuerschmidt@gmail.com>
2020-03-29 23:07:00 -0700

    Update DB script for configs

--- db_scripts/insert_configs.py
+++ db_scripts/insert_configs.py
@@ -5,7 +5,7 @@ sys.path.append('..')
 from slackapp import date_format, db, models
 
 def first_insert():
-    insert_time = datetime.utcnow() - timedelta(weeks=5)
+    insert_time = datetime.utcnow()
     last_refresh = models.Config('LAST_PLAYLIST_REFRESH_TIME', insert_time.strftime(date_format), 'The last time that the playlist was refreshed through a Slack call.')
     last_slash_call = models.Config('LAST_SLASH_COMMAND_TIME', insert_time.strftime(date_format), 'The last time a Slack slash command was called.')
     db.session.add(last_refresh)    
